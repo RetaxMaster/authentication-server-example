81f04990f8817d8958d028b81cd83b848c065370
RetaxMaster <retaxmaster@hotmail.com>
2021-03-12 11:47:11 -0600

    Adicion del endpoint para verificar Tokens

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,14 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+// 1.- Solo el servidor de API debería tener acceso a este endpoint, por lo que sería bueno poner una validación de CORS aquí
+// 2.- El cliente debe mandarle su token al servidor de API a través del Authorization header como Bearer, este debe tener un middleware en todas sus rutas protegidas, este middleware debe hacer el request a este endpoint volviendo a mandar el token en el Authorization Header como Bearer, este endpoint responderá al middleware si el token es válido o no
+Route::post('/oauth/verify-token', function (Request $request) {
+
+    // Este chequeo internamente accede al Authorization header para verificar el token
+    return response()->json([
+        "is_valid_token" => Auth::guard('api')->check()
+    ]);
+
+});
